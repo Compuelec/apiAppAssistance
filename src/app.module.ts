674a3a4b9c88fc16d3d74e305bd39526d64b29e0
Carlos Abarca <soporte@compuelec.cl>
2023-08-31 16:26:52 -0400

    -Update: libraries were updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { UsersModule } from './modules/users/users.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
     UsersModule,
   ],
