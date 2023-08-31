353f2f4411c484b2aafe72b61da489e5f9ee3a99
Carlos Abarca <soporte@compuelec.cl>
2023-08-31 15:59:07 -0400

    -Update: Add Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost', // Para usar en desarrollo para usar en prod y docker, cambiar a 'mysql'
-      port: 3307, // Para usar en desarrollo para usar en prod y docker, cambiar a 3306
-      // host: 'mysql', // Nombre del servicio en el docker-compose.yml (si se usa docker)
-      // port: 3306, // Puerto expuesto en el docker-compose.yml (si se usa docker)
-      username: 'admin',
-      password: 'admin123',
-      database: 'control_asistencia_db',
-      autoLoadEntities: true, // Carga las entidades de forma automática
-      synchronize: false, // Evita que se sincronicen los modelos con la base de datos
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
 import { NestFactory } from '@nestjs/core';
+import 'reflect-metadata';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('API attendance control application')
+    .setDescription(
+      'This is an attendance control application for students, it is also possible to assign subjects.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('doc', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      filter: true,
+      showRequestDuration: true,
+      layout: 'BaseLayout',
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
